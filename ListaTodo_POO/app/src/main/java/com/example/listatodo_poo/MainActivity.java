a19d5e5ac6ed9fa1a26f9283971432c78c9048bf
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-06-16 21:19:22 -0300

    Metodo Remover e Add pronto

--- ListaTodo_POO/app/src/main/java/com/example/listatodo_poo/MainActivity.java
+++ ListaTodo_POO/app/src/main/java/com/example/listatodo_poo/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.listatodo_poo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
@@ -16,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> itensAdapter;
     private ListView list_todo;
     private Button btnAdd;
+    //private EditText editItem;
+    private TextInputEditText editItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +33,45 @@ public class MainActivity extends AppCompatActivity {
         list_todo = findViewById(R.id.list_todo);
         btnAdd = findViewById(R.id.btnAdd);
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        itens = new ArrayList<>();
+        itensAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itens);
+        list_todo.setAdapter( itensAdapter );
+
+        removeItem();
     }
 
-    private void enviarTarefa(View view){
-        
+    private void removeItem() {
+
+        list_todo.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                Toast.makeText(context,"Tarefa removida!", Toast.LENGTH_LONG).show();
+
+                itens.remove(position);
+                itensAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+    private void addItem(View view) {
+
+        editItem = findViewById(R.id.editItem);
+        String itemText = editItem.getText().toString();
+
+        if(!(itemText.equals(""))){
+            itensAdapter.add(itemText);
+            editItem.setText("");
+        } else {
+            Toast.makeText(getApplicationContext(), "Por favor insira uma tarefa.", Toast.LENGTH_LONG).show();
+        }
     }
 }
