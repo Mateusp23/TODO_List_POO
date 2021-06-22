222edfe1acab4d3478b2c612f450400347f7226c
Mateus Paulart <mateusp23@rede.ulbra.br>
2021-06-22 13:01:00 -0300

    finalizando layout com recyclerview e checkbox

--- ListaTodo_POO/app/src/main/java/com/example/listatodo_poo/MainActivity.java
+++ ListaTodo_POO/app/src/main/java/com/example/listatodo_poo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listatodo_poo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -12,16 +14,49 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.listatodo_poo.Adapter.ToDoAdapter;
+import com.example.listatodo_poo.model.ToDoModel;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private RecyclerView rvTarefas;
+    private ToDoAdapter tarefasAdapter;
+
+    private List<ToDoModel> tarefasList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
+        tarefasList = new ArrayList<>();
+
+        rvTarefas = findViewById(R.id.rvTarefas);
+        rvTarefas.setLayoutManager(new LinearLayoutManager(this));
+
+        tarefasAdapter = new ToDoAdapter(this);
+        rvTarefas.setAdapter(tarefasAdapter);
+
+        ToDoModel tarefas = new ToDoModel();
+        tarefas.setTarefa("testando tarefda");
+        tarefas.setStatus(0);
+        tarefas.setId(1);
+
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+        tarefasList.add(tarefas);
+
+        tarefasAdapter.setTarefas(tarefasList);
     }
 }
